e8cc94705e97822a5c25697049ed3fcbe03a5a6f
Boboe16 <114482350+Boboe16@users.noreply.github.com>
2023-02-06 01:46:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -10,34 +10,75 @@ const dictionary_nav = document.getElementById('dictionary-nav').addEventListene
 
 const submit_topic_for_dictionary = document.getElementById('myForm').addEventListener('submit', (event) => {
 event.preventDefault();
+
 const word = document.querySelector('#dictionary-input').value;
-fetch('http://localhost:3000/word', {
-    method: 'POST',
-    body: JSON.stringify({ word }),
-    headers: { 'Content-Type': 'application/json' }
-})
-.then(res => res.json())
-.then(res => Object.entries(res))
-.then(res => Object.entries(res[2][1]))
-.then(res => Object.entries(res[0][1]))
-.then(res => Object.entries(res[0][1]))
-.then(res => Object.entries(res[3]))
-.then(res => Object.entries(res[1]))
-.then(res => res[1][1])
-.then(data => {
-    const cleanText = (data) => {
-    return data.replace(/(<([^>]+)>)/ig,"").replace(/\n/g, "");
-    };
-    document.getElementById('dictionary-output').value = `${cleanText(data)}`
-})
+
+fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+.then(response => response.json())
+.then(data => data[0].meanings[0].definitions[0].definition)
+.then(data => document.getElementById('dictionary-output').value = `${data}`)
 .catch(err => console.error(err));
 });
 
 const submit_topic_for_translator = document.getElementById('myForm2').addEventListener('submit', (event) => { 
     event.preventDefault()
+
     text = document.getElementById('translator-input').value
-    fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=tl|en`)
-    .then(response => response.json())
-    .then(data => document.getElementById('translator-output').value = `${data.responseData.translatedText}`)
-    .catch(error => console.log(error))
+
+    const fetch_translator = (text, to_translate, translate_in) => {
+        fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${to_translate}|${translate_in}`)
+        .then(response => response.json())
+        .then(data => document.getElementById('translator-output').value = `${data.responseData.translatedText}`)
+        .catch(error => console.log(error))
+    }
+
+    const to_translate = document.getElementById('to-translate')
+    const translate_in = document.getElementById('translate-in')
+
+    let languange_to_translate;
+    let translate_to_languange;
+
+    switch(to_translate.value) {
+        case 'English':
+            languange_to_translate = 'en'
+            break
+        case 'Tagalog':
+            languange_to_translate = 'tl'
+            break
+        case 'Spanish':
+            languange_to_translate = 'es'
+            break
+        case 'Mandarin':
+            languange_to_translate = 'zh'
+            break
+        case 'Russian':
+            languange_to_translate = 'ru'
+            break
+        case 'Japanese':
+            languange_to_translate = 'ja'
+            break
+    }
+
+    switch(translate_in.value) {
+        case 'English':
+            translate_to_languange = 'en'
+            break
+        case 'Tagalog':
+            translate_to_languange = 'tl'
+            break
+        case 'Spanish':
+            translate_to_languange = 'es'
+            break
+        case 'Mandarin':
+            translate_to_languange = 'zh'
+            break
+        case 'Russian':
+            translate_to_languange = 'ru'
+            break
+        case 'Japanese':
+            translate_to_languange = 'ja'
+            break
+    }
+
+    fetch_translator(text, languange_to_translate, translate_to_languange)
 })
